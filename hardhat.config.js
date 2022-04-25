2a03301eafb88c85a8a9e45931beefd2def1a505
us3r01 <pdlgmcn@protonmail.com>
2022-04-24 23:50:24 -0500

    Fix grammar

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ module.exports = {
  /**
  * @lang - ES
  * @desc 
- * 1.- Agrega una versión de compilador corecta para 2_Owner.sol
+ * 1.- Agrega una versión de compilador correcta para 2_Owner.sol
  * 2.- Agrega una versión de compilador diferente a 0.4.11 y 0.5.2 pero que sea compatible con el pragma de 1_Owner.sol
  * 3.- Agrega un override para 3_Owner.sol para que lo compile con una versión 0.6.0 
  */
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Descrición
 (ES) hardhat.config.js
-1.- Agrega una versión de compilador corecta para 2_Owner.sol
+1.- Agrega una versión de compilador correcta para 2_Owner.sol
 2.- Agrega una versión de compilador diferente a 0.4.11 y 0.5.2 pero que sea compatible con el pragma de 1_Owner.sol
 3.- Agrega un override para 3_Owner.sol para que lo compile con una versión 0.6.0 
 # Description
